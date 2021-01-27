4dadd89ac39f3896f3acfcdc4be1e6d416729e1f
Gunish Jain <gunish.jain@gmail.com>
2021-01-27 11:49:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/HomePage.dart';
+
+void main()=>runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: "Calculator App",
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new HomePage(),
+    );}
+}
